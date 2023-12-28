4b79584e9e1ff7035e0402b2c70f6198a559d95d
Alejandro López <alejandro.lopez@knoxmediahub.com>
2023-12-28 12:46:31 +0100

    support mac m1

---
+++ setup_docker_compose.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the machine architecture
+machine_arch=$(uname -m)
+
+# Check if it is a Mac M1
+if [ "$machine_arch" == "arm64" ]; then
+    echo "Using docker-compose-mac-m1.yml"
+    ln -f docker-compose-mac-m1.yml docker-compose.yml
+else
+    ln -f docker-compose-linux.yml docker-compose.yml
+    echo "Using docker-compose-linux.yml"
+fi
