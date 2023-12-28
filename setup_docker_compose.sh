ebac51934127c98f53f40589f3c920f790dbdcab
Alejandro López <alejandro.lopez@knoxmediahub.com>
2023-12-28 12:50:41 +0100

    tst

--- setup_docker_compose.sh
+++ setup_docker_compose.sh
@@ -5,9 +5,9 @@ machine_arch=$(uname -m)
 
 # Check if it is a Mac M1
 if [ "$machine_arch" == "arm64" ]; then
-    echo "Using docker-compose-mac-m1.yml"
+    echo "Using docker-compose-mac-m1.yml file"
     ln -f docker-compose-mac-m1.yml docker-compose.yml
 else
     ln -f docker-compose-linux.yml docker-compose.yml
-    echo "Using docker-compose-linux.yml"
+    echo "Using docker-compose-linux.yml file"
 fi
